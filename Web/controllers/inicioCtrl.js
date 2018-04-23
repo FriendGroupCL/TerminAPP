dc647307a241ce444502f9871357ca09054b78a1
jordy098 <jordyp60@gmail.com>
2018-04-22 22:11:23 -0400

    implementación de base de datos terminAPP

--- Web/controllers/inicioCtrl.js
+++ Web/controllers/inicioCtrl.js
@@ -1,8 +1,13 @@
 (function () {
     angular.module('TerminAPP').controller("inicioCtrl", function ($scope, $location, $http) {
    
-   
-    console.log("home controller reached");
-   
+   //http://localhost:8080/facebook/webresources/Users
+        var parametros={};
+        $http.get('http://localhost:8080/facebook/webresources/Users').
+        then(function(response) {
+            parametros = response.name;
+        });
+    //console.log("home controller reached "+parametros);
+        console.log(JSON.stringify(parametros));
 });
 })();
